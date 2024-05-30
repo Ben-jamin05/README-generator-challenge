c25ba7a61fac00df7c75e3d16b2377f765c8379a
Ben Hill <benjaminjhill@icloud.com>
2024-05-30 17:40:21 -0500

    created inquirer prompts and some fs that might work

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,82 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs = require("fs");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is the title of your project?', 
+'Please enter a description of your project',
+'Please enter the installation instructions',
+'Please enter the usage information',
+'Please enter contribution guidelines',
+'Please enter test instructions',
+'Please select a license for your project',
+'What is your github username?',
+'What is a good email to contact you?'
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function init() {
+    inquirer
+      .prompt([
+        {
+            type: "input",
+            message: questions[0],
+            name: "title",
+        },
+        {
+            type: "input",
+            message: questions[1],
+            name: "description",
+        },
+        {
+            type: "input",
+            message: questions[2],
+            name: "installation",
+        },
+        {
+            type: "input",
+            message: questions[3],
+            name: "usage",
+        },
+        {
+            type: "input",
+            message: questions[4],
+            name: "contribute",
+        },
+        {
+            type: "input",
+            message: questions[5],
+            name: "test",
+        },
+        {
+            type: "list",
+            message: questions[6],
+            name: "license",
+            choices: [
+                "MIT",
+                "Apache 2.0",
+                "GNU GPLv3"
+            ],
+        },
+        {
+            type: "input",
+            message: questions[7],
+            name: "github",
+        },
+        {
+            type: "input",
+            message: questions[8],
+            name: "email",
+          },
+      ])
+      .then((data) => {
+        const README = generateMarkdown.generateMarkdownFn(data);
+        const filename = `${data.title.toLowerCase().split(' ').join('_')}.md`;
+    
+        fs.writeFile(filename, README, (err) =>
+          err
+            ? console.log(err)
+            : console.log("success! README file generated as", filename)
+        );
+      });
+    }
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
 init();
